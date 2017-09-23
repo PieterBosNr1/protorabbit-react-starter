f50a81a276dbc25b06f356198686611cfde6f847
Pieter Bos <pieter.bos@gmail.com>
2017-09-23 14:09:56 +0200

    added footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
 
 const Home = ({ children }) => {
   return (
     <div className="container">
       <Header />
       {children}
+      <footer>some footer</footer>
     </div>
   );
 };
